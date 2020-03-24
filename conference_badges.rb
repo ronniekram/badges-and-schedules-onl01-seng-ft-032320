16394d7ca5d21245be61b90ee935533be4b58449
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-24 02:31:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge = []
+  counter = 0 
+  while counter < array.count
+    array.each do |name|
+      badge << "Hello, my name is #{name}."
+      counter += 1
+    end
+  end
+    return badge
+end 
+
+def assign_rooms(array)
+  room = []
+  counter = 0
+  while counter < array.length 
+    array.each_with_index do |name, index|
+      room << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+      counter += 1
+    end
+  end
+   return room
+end  
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |line|
+    puts line 
+  end 
+  assign_rooms(attendees).each do |line|
+    puts line 
+  end 
+end
